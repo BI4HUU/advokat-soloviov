8b86e49abd8e41f9af2ba38ee1d8a10fc484924f
BI4HUU <BI4HUU@yandex.ru>
2020-11-27 18:08:52 +0200

    Add files via upload

--- src/script.js
+++ src/script.js
@@ -1,7 +1,14 @@
 
 document.getElementsByClassName("burger")[0].onclick = function(){togleDropMenu()};
+var menu_items = document.getElementsByClassName("menu-item")
 document.getElementsByClassName("fon-menu")[0].onclick = function(){togleDropMenu()};
 
+for (let menu_item of menu_items) {
+    menu_item.onclick = function(){removeDropMenu()};;
+}
+function removeDropMenu() {
+    document.getElementsByTagName("header")[0].classList.remove("menu-open");
+};
 function togleDropMenu() {
     document.getElementsByTagName("header")[0].classList.toggle("menu-open");
-}
+};
