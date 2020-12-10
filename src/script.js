0b58333050bc34dac613f5a15da57f01437007e2
BI4HUU <BI4HUU@yandex.ru>
2020-12-10 06:41:26 +0200

    Add files via upload

--- src/script.js
+++ src/script.js
@@ -12,3 +12,25 @@ function removeDropMenu() {
 function togleDropMenu() {
     document.getElementsByTagName("header")[0].classList.toggle("menu-open");
 };
+
+
+
+
+var mySwiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    loop: true,
+  
+    slidesPerView: 3,
+    spaceBetween: 30,
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+  })
